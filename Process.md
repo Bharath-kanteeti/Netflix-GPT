59540f952b3d96db36c660da63f8a0b1fb507aa0
Bharath-kanteeti <bharathkanteeti@gmail.com>
2025-02-20 00:28:19 +0530

    first commit

---
+++ Process.md
@@ -0,0 +1,20 @@
+# Netflix-GPT
+ - Created a React App --> npx create-react-app netflix-gpt
+ - Configuring Tailwind CSS --> ('https://v3.tailwindcss.com/docs/guides/create-react-app')
+ 
+
+# Features to build
+ - Home Page
+ - Login / SignUp page
+    - Signin / SignUp form
+    - Redirect to Browse page after logging in
+ - Browse Page (After authentication)
+    - Header
+    - Main Movie
+        - Trailer in background
+        - Title & description
+        - Movie suggetions
+            - List of Movies (n) -- can vertically scrolable
+ - Netflix-GPT
+    - Search bar
+    - Movie suggetions
--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-2xl bg-slate-200 m-2 p-2">
+      Hello Connections let's build new project Namaste-GPT
     </div>
   );
 }
